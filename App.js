653d300f4ade4b3646bfb476ac7d8a739cfa0486
manjot kaur <manjot.kaur9730@gmail.com>
2021-01-09 00:36:52 +0530

    image added

--- App.js
+++ App.js
@@ -26,28 +26,27 @@ const App: () => React$Node = () => {
     }}>
 
       <Timer
-        size={150}
-        strokeWidth={15}
-        duration = {50}
-        color = {'grey'}
-        strokeColor = {'#ffbf00'}
-        fill = {"black"}
-        fillOpacity={0.5}
-        onTimerElapsed = {()=>{console.log('timer finished')}}
-        onClickOption = {()=>{console.log('click option')}}
-        noOfRotations ={1}
-        firstText={true}
-        firstTextColor={'white'}
-        firstTextString={'BIG'}
-        firstTextSize={25}
-        firstX={58}
-        firstY={70}
-        secondText={false}
-        secondTextColor={'white'}
-        secondTextString={'BLIND'}
-        secondTextSize={18}
-        secondX={40}
-        secondY={70}
+        size={150} // size of timer circle
+        strokeWidth={15} // radius for stroke
+        duration = {50} // duration in seconds
+        color = {'grey'} // color of inner stroke
+        strokeColor = {'#ffbf00'} // color for upper stroke
+        fill = {"black"} // color inside circle
+        fillOpacity={0.5} // change it to make transparent
+        onTimerElapsed = {()=>{console.log('timer finished')}} // function called on timer elapsed
+        noOfRotations ={1} // number of rotations to complete above mentioned time
+        firstText={true} // Number of text lines apart from counter
+        firstTextColor={'white'} // Text color
+        firstTextString={'BIG'} // Text string
+        firstTextSize={25} // Text size
+        firstX={58} // x-axis for text
+        firstY={70} // y-axis for text
+        secondText={false} // second text
+        secondTextColor={'white'} // second text color
+        secondTextString={'SMALL'} // text string
+        secondTextSize={18} //  text size
+        secondX={40} //  x-axis for text
+        secondY={70} //  y-axis for text
         ></Timer>
     </View>
  );
